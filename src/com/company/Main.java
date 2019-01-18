cbee79b356e2c8132465aa5a3d742e3715e06654
khadkaraghav <khadkaraghav@gmail.com>
2019-01-18 11:54:53 -0500

    Added nested loop and made to display numbers till 'num'

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,28 +5,45 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int num = 1;
+        int num;
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the number");
         num = Integer.parseInt(input.nextLine());
 
+        System.out.println("1   is not a prime number");
+
         boolean flag = false;
 
-        for (int i = 2; i <= num/2; i++) {
-            if (num % i == 0) {
+        for (int i = 2; i <= num; i++) {
 
-                flag = true;
-                break;
+            flag = false;
+
+
+            for (int j = 2; j < i; j++) {
+
+
+                if (i % j == 0) {
+                    flag = true;
+                    break;
+
+                }
+
             }
-        }
-                if (flag)
-                    System.out.println(num+"\t"+ "is not a prime number");
-                else if (!flag)
-                System.out.println(num+"\t"+ "is a prime Number");
+
+            if (flag)
+                System.out.println(i + "\t" + "is not a prime number");
+            else
+                System.out.println(i + "\t" + "is a prime Number");
 
 
         }
     }
+}
+
+
+
+
+
 
 
 
